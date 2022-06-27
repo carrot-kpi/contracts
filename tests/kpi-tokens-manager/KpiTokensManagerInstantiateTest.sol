eb402e560b680801b28abcdcae790ccfb22e135a
Federico Luzzi <fedeluzzi00@gmail.com>
2022-06-27 16:34:29 +0200

    test: implement tests for kpi tokens manager instantiate function

--- tests/kpi-tokens-manager/KpiTokensManagerInstantiateTest.sol
+++ tests/kpi-tokens-manager/KpiTokensManagerInstantiateTest.sol
@@ -2,6 +2,7 @@ pragma solidity 0.8.15;
 
 import {BaseTestSetup} from "../commons/BaseTestSetup.sol";
 import {OraclesManager} from "../../contracts/OraclesManager.sol";
+import {IERC20KPIToken} from "../../contracts/interfaces/kpi-tokens/IERC20KPIToken.sol";
 import {Clones} from "oz/proxy/Clones.sol";
 
 /// SPDX-License-Identifier: GPL-3.0-or-later
@@ -9,39 +10,80 @@ import {Clones} from "oz/proxy/Clones.sol";
 /// @dev Tests template instantiation in KPI tokens manager.
 /// @author Federico Luzzi - <federico.luzzi@protonmail.com>
 contract KpiTokensManagerInstantiateTest is BaseTestSetup {
-    function testFailNotFromCreatedKpiToken() external {
+    function testFailNotFromFactory() external {
         // FIXME: why does this fail if I uncomment stuff?
         vm.expectRevert(); /* abi.encodeWithSignature("Forbidden()") */
         oraclesManager.instantiate(address(this), 0, bytes(""));
     }
 
-    function testSuccessManualRealityOracle() external {
-        bytes memory _initializationData = abi.encode(
-            address(2), // fake reality.eth address
-            address(this), // arbitrator
-            0, // template id
-            "a", // question
-            200, // question timeout
-            block.timestamp + 200 // expiry
+    function testSuccessERC20() external {
+        IERC20KPIToken.Collateral[]
+            memory _collaterals = new IERC20KPIToken.Collateral[](1);
+        _collaterals[0] = IERC20KPIToken.Collateral({
+            token: address(firstErc20),
+            amount: 2,
+            minimumPayout: 1
+        });
+        bytes memory _erc20KpiTokenInitializationData = abi.encode(
+            _collaterals,
+            "Test",
+            "TST",
+            100 ether
         );
-        address _predictedInstanceAddress = Clones.predictDeterministicAddress(
-            address(manualRealityOracleTemplate),
-            keccak256(abi.encodePacked(address(this), _initializationData)),
-            address(oraclesManager)
-        );
+
+        string memory _description = "test";
+        string memory _question = "test?";
+
+        address _reality = address(42);
         vm.mockCall(
-            address(factory),
+            _reality,
             abi.encodeWithSignature(
-                "allowOraclesCreation(address)",
-                address(this)
+                "askQuestion(uint256,string,address,uint32,uint32,uint256)"
             ),
-            abi.encode(true)
+            abi.encode(bytes32("question id"))
         );
-        address _instance = oraclesManager.instantiate(
+        bytes memory _manualRealityOracleInitializationData = abi.encode(
+            _reality,
             address(this),
-            0,
-            _initializationData
+            1,
+            _question,
+            60,
+            block.timestamp + 60
         );
+        IERC20KPIToken.OracleData[]
+            memory _oracleDatas = new IERC20KPIToken.OracleData[](1);
+        _oracleDatas[0] = IERC20KPIToken.OracleData({
+            templateId: 0,
+            lowerBound: 0,
+            higherBound: 1,
+            weight: 1,
+            data: _manualRealityOracleInitializationData
+        });
+        bytes memory _oraclesInitializationData = abi.encode(
+            _oracleDatas,
+            false
+        );
+
+        address _predictedInstanceAddress = Clones.predictDeterministicAddress(
+            address(erc20KpiTokenTemplate),
+            keccak256(
+                abi.encodePacked(
+                    _description,
+                    _erc20KpiTokenInitializationData,
+                    _oraclesInitializationData
+                )
+            ),
+            address(kpiTokensManager)
+        );
+
+        vm.prank(address(factory));
+        address _instance = kpiTokensManager.instantiate(
+            0,
+            _description,
+            _erc20KpiTokenInitializationData,
+            _oraclesInitializationData
+        );
+
         assertEq(_instance, _predictedInstanceAddress);
         vm.clearMockedCalls();
     }
