1f6004e3d1d1c37dfa5edea6ad59460f6a4fdc51
Federico Luzzi <fedeluzzi00@gmail.com>
2022-10-01 00:02:23 +0100

    feat: remove erc20 kpi token deployment from deploy script

--- scripts/Deploy.sol
+++ scripts/Deploy.sol
@@ -1,10 +1,5 @@
 pragma solidity 0.8.17;
 
-import {Clones} from "oz/proxy/Clones.sol";
-import {TransparentUpgradeableProxy} from "oz/proxy/transparent/TransparentUpgradeableProxy.sol";
-import {ProxyAdmin} from "oz/proxy/transparent/ProxyAdmin.sol";
-import {ERC20KPIToken} from "../contracts/kpi-tokens/ERC20KPIToken.sol";
-import {RealityV3Oracle} from "../contracts/oracles/RealityV3Oracle.sol";
 import {OraclesManager1} from "../contracts/oracles-managers/OraclesManager1.sol";
 import {KPITokensManager1} from "../contracts/kpi-tokens-managers/KPITokensManager1.sol";
 import {KPITokensFactory} from "../contracts/KPITokensFactory.sol";
@@ -42,34 +37,12 @@ contract Deploy is Script {
             address(_kpiTokensManager)
         );
 
-        ERC20KPIToken _erc20KpiTokenTemplate = new ERC20KPIToken();
-        console2.log(
-            "ERC20 KPI token template deployed at address: ",
-            address(_erc20KpiTokenTemplate)
-        );
-
-        _kpiTokensManager.addTemplate(
-            address(_erc20KpiTokenTemplate),
-            ERC20_KPI_TOKEN_SPECIFICATION
-        );
-
         OraclesManager1 _oraclesManager = new OraclesManager1(
             address(_factory)
         );
         console2.log(
             "Oracles manager deployed at address: ",
             address(_oraclesManager)
-        );
-
-        RealityV3Oracle _realityV3OracleTemplate = new RealityV3Oracle();
-        console2.log(
-            "Manual Reality oracle template deployed at address: ",
-            address(_realityV3OracleTemplate)
-        );
-
-        _oraclesManager.addTemplate(
-            address(_realityV3OracleTemplate),
-            MANUAL_REALITY_ETH_ORACLE_SPECIFICATION
         );
 
         _factory.setKpiTokensManager(address(_kpiTokensManager));
