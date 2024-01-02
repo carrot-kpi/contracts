e61d04aff81ae9f94ce514483b36611c27da92d8
Federico Luzzi <federico.luzzi@proton.me>
2024-01-02 14:41:59 +0100

    feat: add base kpi token preset contract (#19)
    Co-authored-by: luzzifoss <fedeluzzi00@gmail.com>

--- test/oracles-manager/OraclesManagerInitializeTest.sol
+++ test/oracles-manager/OraclesManagerInitializeTest.sol
@@ -21,8 +21,7 @@ contract OraclesManagerInitializeTest is BaseTestSetup {
         OraclesManager _manager = new OraclesManager();
         vm.expectRevert(abi.encodeWithSignature("ZeroAddressFactory()"));
         new ERC1967Proxy(
-            address(_manager),
-            abi.encodeWithSelector(BaseTemplatesManager.initialize.selector, owner, address(0), address(1))
+            address(_manager), abi.encodeWithSelector(BaseTemplatesManager.initialize.selector, owner, address(0))
         );
     }
 }
